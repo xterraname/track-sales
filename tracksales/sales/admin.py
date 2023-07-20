8b02835be1ad25ac54b11358083dc888f4d367ac
Jamoliddin <jamol1741@gmail.com>
2023-07-20 17:56:58 +0500

    Create models

--- tracksales/sales/admin.py
+++ tracksales/sales/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'birth_date')
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'birth_date')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'quantity', 'price')
+    list_editable = ('quantity', 'price')
+
+
+@admin.register(ProductOrder)
+class ProductOrderAdmin(admin.ModelAdmin):
+    list_display = ('product', 'quantity')
+    list_editable = ('quantity', )
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('client', 'employee', 'date')
--- tracksales/sales/models.py
+++ tracksales/sales/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.db.models import Sum, F
 
-# Create your models here.
+
+class Employee(models.Model):
+    full_name = models.CharField(max_length=200)
+    birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.full_name
+
+
+class Client(models.Model):
+    full_name = models.CharField(max_length=200)
+    birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.full_name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=500, unique=True)
+    quantity = models.IntegerField(default=0)
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class ProductOrder(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.IntegerField(default=1)
+
+
+class Order(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.SET_NULL, null=True, blank=True)
+    products = models.ManyToManyField(ProductOrder)
+    employee = models.ForeignKey(Employee, on_delete=models.PROTECT)
+    
+    date = models.DateField(auto_now_add=True)
+
+    def price(self):
+        total_amount = self.products.aggregate(
+            total_amount=Sum(F('quantity') * F('product__price'))
+        )['total_amount']
+
+        total_amount = total_amount or 0
+
+        return total_amount
